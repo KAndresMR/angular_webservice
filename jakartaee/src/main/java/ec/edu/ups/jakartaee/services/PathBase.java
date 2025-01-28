0a1b5059c103c5ba96fbed621ad93d8f43d91320
KAndresMR <morochoandres12@gmail.com>
2025-01-27 21:42:47 -0500

    Rama test: creada

--- jakartaee/src/main/java/ec/edu/ups/jakartaee/services/PathBase.java
+++
@@ -1,9 +0,0 @@
-package ec.edu.ups.jakartaee.services;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class PathBase extends Application{
-
-}
